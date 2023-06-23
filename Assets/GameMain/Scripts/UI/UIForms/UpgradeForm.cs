fb6df1f2a96da941051676c7f56f9be417193a83
MoZeQing <48617218+MoZeQing@users.noreply.github.com>
2023-06-23 16:29:59 +0800

    Update Upgarde
    1.更新建筑升级的UI
    2.更新升级UI的脚本（但未实现）

---
+++ Assets/GameMain/Scripts/UI/UIForms/UpgradeForm.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace GameMain
+{
+    public class UpgradeForm :UIFormLogic
+    {
+
+    }
+}
